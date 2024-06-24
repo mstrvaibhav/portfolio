4c1fc0589e46e2e7cc9ac4f24ec03f633bd1b211
realvaibhava <realvaibhava@gmail.com>
2024-06-24 22:30:59 +0530

    changed metadata v2

--- app/layout.js
+++ app/layout.js
@@ -16,19 +16,19 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>Check X2</title>
-        <meta property="og:title" content="Design Check" />
-        <meta property="og:description" content="Description of my Next.js website" />
+        <title>realvaibhava ✦ designer ✦ artist ✦ dreamer</title>
+        <meta property="og:title" content="realvaibhava ✦ designer ✦ artist ✦ dreamer" />
+        <meta property="og:description" content="I am designer currently crafting cross platform consumer experiences at MakeMyTrip flights team." />
         <meta property="og:url" content="https://www.vaibhava.me/" />
         <meta property="og:image" content="https://www.vaibhava.me/preview.jpg" />
         <meta property="og:image:width" content="4800" />
         <meta property="og:image:height" content="3200" />
-        <meta property="og:image:alt" content="About Artist Vaibhav" />
+        <meta property="og:image:alt" content="realvaibhava ✦ designer ✦ artist ✦ dreamer" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content="https://www.vaibhava.me/preview.jpg" />
         <meta name="twitter:image:width" content="4800" />
         <meta name="twitter:image:height" content="3200" />
-        <meta property="twitter:image:alt" content="About Designer Vaibhav" />
+        <meta property="twitter:image:alt" content="realvaibhava ✦ designer ✦ artist ✦ dreamer" />
       </head>
       <body className={inter.className} style={{ width: "100%", display: "flex" }}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
