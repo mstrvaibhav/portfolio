f771a7873db4fe2b56b17186519c0d1b6cbedb19
realvaibhava <realvaibhava@gmail.com>
2024-06-24 22:14:39 +0530

    changed meta tags

--- app/layout.js
+++ app/layout.js
@@ -16,13 +16,19 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <title>Hello!</title>
-        <meta property="og:title" content="✦ realvaibhava ✦" />
+        <title>Check X2</title>
+        <meta property="og:title" content="Design Check" />
         <meta property="og:description" content="Description of my Next.js website" />
-        <meta property="og:image" content="https://www.vaibhava.me/preview.png" />
         <meta property="og:url" content="https://www.vaibhava.me/" />
+        <meta property="og:image" content="https://www.vaibhava.me/preview.jpg" />
+        <meta property="og:image:width" content="4800" />
+        <meta property="og:image:height" content="3200" />
+        <meta property="og:image:alt" content="About Artist Vaibhav" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://www.vaibhava.me/preview.png" />
+        <meta name="twitter:image" content="https://www.vaibhava.me/preview.jpg" />
+        <meta name="twitter:image:width" content="4800" />
+        <meta name="twitter:image:height" content="3200" />
+        <meta property="twitter:image:alt" content="About Designer Vaibhav" />
       </head>
       <body className={inter.className} style={{ width: "100%", display: "flex" }}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
